5661cecae0b0f2081e2cf6c3e94c0163b177106d
KoniBelen <konibelen12@gmail.com>
2019-10-03 12:49:00 -0300

    Se agrega ServiceImpl para Derecho

---
+++ src/main/java/com/backendcementeriode/pinto/models/Service/classImpl/DechoServiceImpl.java
@@ -0,0 +1,50 @@
+package com.backendcementeriode.pinto.models.Service.classImpl;
+
+
+import com.backendcementeriode.pinto.models.Dao.IDerechoDao;
+import com.backendcementeriode.pinto.models.Entity.Derecho;
+import com.backendcementeriode.pinto.models.Service.SeviceInterface.IDerechoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DechoServiceImpl implements IDerechoService {
+
+    @Autowired
+    public IDerechoDao derechoDao;
+
+
+    @Override
+    public List<Derecho> findAll() {
+        return derechoDao.findAll();
+    }
+
+    @Override
+    public Derecho save(Derecho derecho) {
+        return derechoDao.save(derecho);
+    }
+
+    @Override
+    public Optional<Derecho> findOne(long id) {
+        return derechoDao.findById(id);
+    }
+
+    @Transactional
+    public void delete(Derecho derecho) {
+        derechoDao.delete(derecho);
+    }
+
+    @Transactional
+    public void deletebyID(long id) {
+        derechoDao.deleteById(id);
+    }
+
+    @Transactional
+    public Derecho findById(long id) {
+        return derechoDao.findById(id).orElse(null);
+    }
+}
