5d782f3cbd4a22bace55cf20d38fbc70738c10a6
MChavezSaa <matroxmega@gmail.com>
2019-10-08 22:49:41 -0300

    patioController listo

--- src/main/java/com/backendcementeriode/pinto/controllers/patioController.java
+++ src/main/java/com/backendcementeriode/pinto/controllers/patioController.java
@@ -3,14 +3,19 @@ package com.backendcementeriode.pinto.controllers;
 
 import com.backendcementeriode.pinto.models.Entity.Funcionario;
 import com.backendcementeriode.pinto.models.Entity.Patio;
+import com.backendcementeriode.pinto.models.Entity.Terreno;
 import com.backendcementeriode.pinto.models.Service.classImpl.PatioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -23,6 +28,44 @@ public class patioController {
     public List<Patio> findAll() {
         List<Patio> all = patioService.findAll();
         return all;
+    }
+
+    @PostMapping(value = "/savePatio")
+    @ResponseStatus(value = CREATED)
+    public ResponseEntity<?> create(@RequestBody Patio patio) {
+
+        Patio patio1 = null;
+        Map<String, Object> response = new HashMap<String, Object>();
+
+        try {
+            patio1 = patioService.save(patio);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al realizar el insert en la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El patio ha sido creado con éxito!");
+        response.put("Funcionario", patio1);
+
+        return new ResponseEntity<Map<String, Object>>(response, OK);
+    }
+
+    @RequestMapping(value = "/DeletePatio/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+
+        Map<String, Object> response = new HashMap<String, Object>();
+        try {
+            patioService.deletebyID(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al deshabilitar el patio de la base de datos");
+            response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El patio fue deshabilitado con éxito!");
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+
     }
 
 
