bfbe005b140d04bbc98c799040397fd6013b8a4b
MChavezSaa <matroxmega@gmail.com>
2019-11-03 22:07:14 -0300

    Actualizacion al properties y se creo DifuntoController

---
+++ src/main/java/com/backendcementeriode/pinto/controllers/DifuntoController.java
@@ -0,0 +1,139 @@
+package com.backendcementeriode.pinto.controllers;
+
+
+import com.backendcementeriode.pinto.models.Entity.Cliente;
+import com.backendcementeriode.pinto.models.Entity.Difunto;
+import com.backendcementeriode.pinto.models.Service.classImpl.DifuntoServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.springframework.http.HttpStatus.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+public class DifuntoController {
+
+    @Autowired
+    private DifuntoServiceImpl difuntoService;
+
+    private final Logger log = LoggerFactory.getLogger(DifuntoController.class);
+
+
+    ////-------------- Listar Difuntos ---------------------////
+    @Secured("ROLE_ADMIN")
+    @RequestMapping(value = "/listDifuntos", method = RequestMethod.GET)
+    public List<Difunto> findAll(){
+        List<Difunto> all = difuntoService.findAll();
+        return all;
+    }
+
+    ////-------------- Guardar Clientes ---------------------////
+    @Secured("ROLE_ADMIN")
+    @PostMapping(value = "/saveDifunto")
+    @ResponseStatus(value = CREATED)
+    public ResponseEntity<?> create(@RequestBody Difunto difunto){
+        difunto.setEstadoDifunto(true);
+        Difunto difunto1= null;
+        Map<String,Object> response =new HashMap<String, Object>();
+
+        try {
+            difunto1=difuntoService.save(difunto);
+        }catch(DataAccessException e) {
+            response.put("mensaje","Error al realizar el insert en la base de datos");
+            response.put("error",e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String,Object>>(response, INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje","El difunto ha sido creado con éxito!");
+        response.put("Cliente",difunto1);
+
+        return new ResponseEntity<Map<String,Object>>(response, OK);
+
+
+    }
+
+    ////-------------- Subir Certificado Defuncion ---------------------////
+    @PostMapping("/DifuntoUpload")
+    public ResponseEntity<?>upload(@RequestParam("archivo") MultipartFile archivo, @RequestParam("id") Long id) throws IOException {
+        Map<String, Object> response = new HashMap<>();
+        Difunto difunto = difuntoService.findById(id);
+
+        if(archivo.isEmpty()){
+            String nombreArchivo = UUID.randomUUID()+"_"+archivo.getOriginalFilename().replace(" ","" );
+
+            Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+
+            log.info(rutaArchivo.toString());
+
+            try{
+                Files.copy(archivo.getInputStream(), rutaArchivo );
+            }catch (Exception e){
+                response.put("Mennsaje", "Error al subir el certificado");
+                response.put("error",e.getMessage().concat(": ").concat(e.getCause().getMessage()));
+                return new ResponseEntity<Map<String,Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            String nombreArchivoanterior= difunto.getCertificado_Defuncion();
+
+            if(nombreArchivoanterior != null && nombreArchivoanterior.length()>0 ){
+                Path rutaArchivoAnterior = Paths.get("uploads").resolve(nombreArchivoanterior).toAbsolutePath();
+                File archivoAnterior = rutaArchivoAnterior.toFile();
+                if (archivoAnterior.exists() &&  archivoAnterior.canRead()){
+                    archivoAnterior.delete();
+                }
+            }
+
+            difunto.setCertificado_Defuncion(nombreArchivo);
+            difuntoService.save(difunto);
+            response.put("Difunto", difunto);
+            response.put("Mennsaje", "Has subido correctamente el certificado "+ nombreArchivo);
+        }
+        return new ResponseEntity<Map<String,Object>>(response, CREATED);
+    }
+
+    @GetMapping("/uploads/img/{nombreArchivo :.+}")
+    public ResponseEntity<Resource> verArchivo(@PathVariable String nombreArchivo) throws MalformedURLException {
+
+        Path rutaArchivo = Paths.get("uploads").resolve(nombreArchivo).toAbsolutePath();
+        Resource recurso = null;
+
+        log.info(rutaArchivo.toString());
+
+        try {
+            recurso = new UrlResource(rutaArchivo.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        if (!recurso.exists() && recurso.isReadable()) {
+            throw new RuntimeException("No se pudo cargar el archivo "+ nombreArchivo);
+        }
+        HttpHeaders cabecera = new HttpHeaders();
+        //forzar descarga de archivo
+        cabecera.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+ recurso.getFilename()+"\"");
+
+
+        return new ResponseEntity<Resource> (recurso, cabecera, HttpStatus.OK);
+    }
+
+
+}
